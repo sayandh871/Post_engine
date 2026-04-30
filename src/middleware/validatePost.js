57113a8bb2639e64e89a5ea9f8a93cbb8edf1ffc
sayandh <sayandh871@gmail.com>
2026-04-30 10:36:44 +0530

    feat: implement request validation layer using Joi
    - created validation schemas for create and update post
    - built reusable validation middleware (validate)
    - added route-level validation integration
    - enforced enum-based category validation
    - added error handling with detailed messages
    - prevented unknown fields using Joi configuration

--- src/middleware/validatePost.js
+++ src/middleware/validatePost.js
@@ -1,12 +1,22 @@
-export function validatePost(req,res,next){
 
-    let {title,content} = req.body;
-    if(!title){
-        return res.status(400).send("title required")
-    }
-    if(!content){
-        return res.status(400).send("content required")
 
+export function validate(schema) {
+  return function (req, res, next) {
+    const { value, error } = schema.validate(req.body,{
+      abortEarly: false,   // collect all errors
+      stripUnknown: true,  // remove extra fields
+    });
+
+    if (error) {
+      const message = error.details.map(err => err.message);
+
+      return res.status(400).json({
+        success: false,
+        errors: message
+      })
     }
+
+    req.body = value;
     next();
+  };
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,15 +1,16 @@
 import express from "express"
 import {createpost,getAllPost,getSinglePost,deletePostById,updatePostById} from "../controllers/postController.js"
-import {validatePost} from "../middleware/validatePost.js"
+import {validate} from "../middleware/validatePost.js"
 import asyncWrapper from "../middleware/asyncWrapper.js"
+import { createPostSchema, updatePostSchema } from "../validations/postValidation.js"
 
 const router = express.Router()
 
 
-router.post("/",validatePost, asyncWrapper(createpost))
+router.post("/",validate(createPostSchema), asyncWrapper(createpost))
 router.get("/", asyncWrapper(getAllPost))
 router.get("/:id", asyncWrapper(getSinglePost))
 router.delete("/:id", asyncWrapper(deletePostById))
-router.put("/:id", asyncWrapper(updatePostById))
+router.put("/:id", validate(updatePostSchema), asyncWrapper(updatePostById))
 
 export default router;
