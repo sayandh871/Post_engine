9e226be76a5f92a142a2295e84848e23cc81fba3
sayandh <sayandh871@gmail.com>
2026-05-06 09:12:47 +0530

    feat(auth): implement authentication routes for user registration and login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { connectDB } from "./src/config/db.js";
 import dotenv from "dotenv"
 import postRoutes from "./src/routes/postsRoutes.js"
 import errorHandler from "./src/middleware/errorHandler.js";
+import authRoutes from "./src/routes/authRoutes.js"
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(express.json())
 app.use("/posts",postRoutes)
+app.use("/api/auth",authRoutes)
 
 
 app.use(errorHandler)
